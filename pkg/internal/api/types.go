4a8e90fe7dfb856fcef4d21543b59a59576b39e7
Haroun Ahmad <HarounAhmad02@gmail.com>
2025-08-09 16:22:51 +0200

    add types

---
+++ pkg/internal/api/types.go
@@ -0,0 +1,45 @@
+package api
+
+type Op string
+
+const (
+	OpHealth        Op = "HEALTH"
+	OpSign          Op = "SIGN"
+	OpGenKeyAndSign Op = "GENKEY_AND_SIGN"
+	OpRevoke        Op = "REVOKE"
+	OpGetCRL        Op = "GET_CRL"
+)
+
+type Profile string
+
+const (
+	ProfileClient Profile = "client"
+	ProfileServer Profile = "server"
+)
+
+type KeyType string
+
+const (
+	KeyRSA4096 KeyType = "rsa4096"
+	KeyEd25519 KeyType = "ed25519"
+)
+
+type Request struct {
+	Op         Op      `json:"op"`
+	CN         string  `json:"cn,omitempty"`
+	Profile    Profile `json:"profile,omitempty"`
+	CSRPEM     string  `json:"csr_pem,omitempty"`
+	KeyType    KeyType `json:"key_type,omitempty"`
+	Passphrase string  `json:"passphrase,omitempty"`
+	Serial     string  `json:"serial,omitempty"`
+	Reason     string  `json:"reason,omitempty"`
+}
+
+type Response struct {
+	CertPEM   string `json:"cert_pem,omitempty"`
+	KeyPEMEnc string `json:"key_pem_encrypted,omitempty"`
+	CRLPEM    string `json:"crl_pem,omitempty"`
+	Serial    string `json:"serial,omitempty"`
+	NotAfter  string `json:"not_after,omitempty"`
+	Error     string `json:"err,omitempty"`
+}
