0f5c4eb3100daf9979461bec31f70890b5a28a8d
Haroun Ahmad <HarounAhmad02@gmail.com>
2025-08-09 18:48:52 +0200

    update main file to fix mistake, add update app file

---
+++ cmd/vpn-certctl/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"time"
+
+	"github.com/HarounAhmad/vpn-certd/internal/api"
+)
+
+func main() {
+	var socket, op, cn, profile, keyType, pass, csr, serial, reason string
+	flag.StringVar(&socket, "socket", "./dist/run/vpn-certd.sock", "unix socket")
+	flag.StringVar(&op, "op", "HEALTH", "op: HEALTH|SIGN|GENKEY_AND_SIGN|REVOKE|GET_CRL")
+	flag.StringVar(&cn, "cn", "", "common name")
+	flag.StringVar(&profile, "profile", "client", "profile: client|server")
+	flag.StringVar(&keyType, "key-type", "rsa4096", "key type: rsa4096|ed25519")
+	flag.StringVar(&pass, "passphrase", "", "passphrase (for GENKEY_AND_SIGN)")
+	flag.StringVar(&csr, "csr", "", "PEM CSR (for SIGN)")
+	flag.StringVar(&serial, "serial", "", "serial (for REVOKE)")
+	flag.StringVar(&reason, "reason", "", "reason (for REVOKE)")
+	flag.Parse()
+
+	req := api.Request{
+		Op:         api.Op(op),
+		CN:         cn,
+		Profile:    api.Profile(profile),
+		KeyType:    api.KeyType(keyType),
+		Passphrase: pass,
+		CSRPEM:     csr,
+		Serial:     serial,
+		Reason:     reason,
+	}
+
+	b, _ := json.Marshal(req)
+	conn, err := net.DialTimeout("unix", socket, 3*time.Second)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "dial:", err)
+		os.Exit(2)
+	}
+	defer conn.Close()
+	if _, err := conn.Write(append(b, '\n')); err != nil {
+		fmt.Fprintln(os.Stderr, "write:", err)
+		os.Exit(2)
+	}
+	dec := json.NewDecoder(conn)
+	var resp api.Response
+	if err := dec.Decode(&resp); err != nil {
+		fmt.Fprintln(os.Stderr, "decode:", err)
+		os.Exit(2)
+	}
+	enc := json.NewEncoder(os.Stdout)
+	enc.SetIndent("", "  ")
+	_ = enc.Encode(resp)
+}
--- internal/app/app.go
+++ internal/app/app.go
@@ -3,11 +3,14 @@ package app
 import (
 	"context"
 	"errors"
+	"log/slog"
+	"time"
+
 	"github.com/HarounAhmad/vpn-certd/internal/api"
 	"github.com/HarounAhmad/vpn-certd/internal/constants"
 	"github.com/HarounAhmad/vpn-certd/internal/server/unixjson"
-	"log/slog"
-	"time"
+	"github.com/HarounAhmad/vpn-certd/internal/validate"
+	"github.com/HarounAhmad/vpn-certd/internal/xerr"
 )
 
 type App struct {
@@ -18,16 +21,49 @@ func New(log *slog.Logger) *App {
 	return &App{Log: log}
 }
 
-func (a *App) Handler() unixjson.Handler {
-	return a
-}
+func (a *App) Handler() unixjson.Handler { return a }
 
 func (a *App) Handle(ctx context.Context, req api.Request) (api.Response, error) {
 	switch req.Op {
 	case api.OpHealth:
 		return api.Response{Serial: "ok", NotAfter: time.Now().UTC().Format(time.RFC3339)}, nil
-	case api.OpSign, api.OpGenKeyAndSign, api.OpRevoke, api.OpGetCRL:
-		return api.Response{}, errors.New("not_implemented_step1")
+
+	case api.OpSign:
+		if err := validate.CN(req.CN); err != nil {
+			return api.Response{}, xerr.Bad("cn")
+		}
+		if err := validate.Profile(req.Profile); err != nil {
+			return api.Response{}, xerr.Bad("profile")
+		}
+		if err := validate.CSR(req.CSRPEM); err != nil {
+			return api.Response{}, xerr.Bad("csr_pem")
+		}
+		return api.Response{}, xerr.NotImplemented("sign")
+
+	case api.OpGenKeyAndSign:
+		if err := validate.CN(req.CN); err != nil {
+			return api.Response{}, xerr.Bad("cn")
+		}
+		if err := validate.Profile(req.Profile); err != nil {
+			return api.Response{}, xerr.Bad("profile")
+		}
+		if err := validate.KeyType(req.KeyType); err != nil {
+			return api.Response{}, xerr.Bad("key_type")
+		}
+		if err := validate.Passphrase(req.Passphrase); err != nil {
+			return api.Response{}, xerr.Bad("passphrase")
+		}
+		return api.Response{}, xerr.NotImplemented("genkey_and_sign")
+
+	case api.OpRevoke:
+		if req.Serial == "" {
+			return api.Response{}, xerr.Bad("serial")
+		}
+		return api.Response{}, xerr.NotImplemented("revoke")
+
+	case api.OpGetCRL:
+		return api.Response{}, xerr.NotImplemented("get_crl")
+
 	default:
 		return api.Response{}, errors.New("unknown_op")
 	}
