91669a785c5954b44bef99967f5c54fde1e068fc
Haroun Ahmad <HarounAhmad02@gmail.com>
2025-08-10 11:41:31 +0200

    add crl output path

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,8 +2,9 @@ package config
 
 import (
 	"flag"
-	"github.com/HarounAhmad/vpn-certd/internal/constants"
 	"os"
+
+	"github.com/HarounAhmad/vpn-certd/internal/constants"
 )
 
 type Config struct {
@@ -11,6 +12,8 @@ type Config struct {
 	PKIDir     string
 	StateDir   string
 	LogLevel   string
+	PolicyPath string
+	CRLOutPath string
 }
 
 func Load() Config {
@@ -20,11 +23,15 @@ func Load() Config {
 	pki := getenvDefault(constants.EnvPKIDir, constants.DefaultPKIDir)
 	state := getenvDefault(constants.EnvStateDir, constants.DefaultStateDir)
 	level := getenvDefault(constants.EnvLogLevel, constants.DefaultLogLevel)
+	policy := getenvDefault(constants.EnvPolicyPath, constants.DefaultPolicy)
+	crlout := getenvDefault(constants.EnvCRLOutPath, constants.DefaultCRLOut)
 
 	flag.StringVar(&c.SocketPath, "socket", socket, "UNIX socket path")
 	flag.StringVar(&c.PKIDir, "pki", pki, "PKI directory (intermediate CA)")
 	flag.StringVar(&c.StateDir, "state", state, "State directory")
 	flag.StringVar(&c.LogLevel, "log-level", level, "log level: debug|info|warn|error")
+	flag.StringVar(&c.PolicyPath, "policy", policy, "policy YAML file path")
+	flag.StringVar(&c.CRLOutPath, "crl-out", crlout, "CRL deployment path for OpenVPN")
 	flag.Parse()
 
 	return c
