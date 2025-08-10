7da35acb21fb4e188091540b8f69d28cc68576be
Haroun Ahmad <HarounAhmad02@gmail.com>
2025-08-10 12:20:46 +0200

    admin cli support buildbundle op and emit the zip to file

--- cmd/vpn-certctl/main.go
+++ cmd/vpn-certctl/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -12,7 +13,9 @@ import (
 )
 
 func main() {
-	var socket, op, cn, profile, keyType, pass, csr, serial, reason string
+	var socket, op, cn, profile, keyType, pass, csr, serial, bundleCN, bundleRemote, bundleProto, bundleOut, reason string
+	var bundlePort int
+	var bundleIncludeKey bool
 	flag.StringVar(&socket, "socket", "./dist/run/vpn-certd.sock", "unix socket")
 	flag.StringVar(&op, "op", "HEALTH", "op: HEALTH|SIGN|GENKEY_AND_SIGN|REVOKE|GET_CRL|LIST_ISSUED")
 	flag.StringVar(&cn, "cn", "", "common name")
@@ -22,6 +25,12 @@ func main() {
 	flag.StringVar(&csr, "csr", "", "PEM CSR (for SIGN)")
 	flag.StringVar(&serial, "serial", "", "serial (for REVOKE)")
 	flag.StringVar(&reason, "reason", "", "reason (for REVOKE)")
+	flag.StringVar(&bundleCN, "bundle-cn", "", "BUILD_BUNDLE: CN")
+	flag.StringVar(&bundleRemote, "bundle-remote", "", "BUILD_BUNDLE: remote host")
+	flag.IntVar(&bundlePort, "bundle-port", 1194, "BUILD_BUNDLE: remote port")
+	flag.StringVar(&bundleProto, "bundle-proto", "udp", "BUILD_BUNDLE: proto")
+	flag.BoolVar(&bundleIncludeKey, "bundle-include-key", true, "BUILD_BUNDLE: include key if cached")
+	flag.StringVar(&bundleOut, "bundle-out", "", "BUILD_BUNDLE: output zip path")
 	flag.Parse()
 
 	req := api.Request{
@@ -33,6 +42,16 @@ func main() {
 		CSRPEM:     csr,
 		Serial:     serial,
 		Reason:     reason,
+	}
+
+	if op == "BUILD_BUNDLE" {
+		req.Bundle = &api.BundleReq{
+			CN:         bundleCN,
+			IncludeKey: bundleIncludeKey,
+			RemoteHost: bundleRemote,
+			RemotePort: bundlePort,
+			Proto:      bundleProto,
+		}
 	}
 
 	b, _ := json.Marshal(req)
@@ -52,6 +71,24 @@ func main() {
 		fmt.Fprintln(os.Stderr, "decode:", err)
 		os.Exit(2)
 	}
+	if op == "BUILD_BUNDLE" && resp.ZipB64 != "" {
+		out := bundleOut
+		if out == "" {
+			out = fmt.Sprintf("./dist/%s.zip", bundleCN)
+		}
+		b, err := base64.StdEncoding.DecodeString(resp.ZipB64)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "decode zip:", err)
+			os.Exit(2)
+		}
+		if err := os.WriteFile(out, b, 0o600); err != nil {
+			fmt.Fprintln(os.Stderr, "write zip:", err)
+			os.Exit(2)
+		}
+		fmt.Println(out)
+		return
+	}
+
 	enc := json.NewEncoder(os.Stdout)
 	enc.SetIndent("", "  ")
 	_ = enc.Encode(resp)
