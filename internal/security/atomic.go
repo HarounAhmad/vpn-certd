7c9b9d9efdf360b8c9c9e179c12182810d16018a
Haroun Ahmad <HarounAhmad02@gmail.com>
2025-08-10 11:46:03 +0200

    atomic write utility for CRL deployment

---
+++ internal/security/atomic.go
@@ -0,0 +1,18 @@
+package security
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func AtomicWrite(path string, data []byte, perm os.FileMode) error {
+	dir := filepath.Dir(path)
+	if err := os.MkdirAll(dir, 0o755); err != nil {
+		return err
+	}
+	tmp := path + ".tmp"
+	if err := os.WriteFile(tmp, data, perm); err != nil {
+		return err
+	}
+	return os.Rename(tmp, path)
+}
